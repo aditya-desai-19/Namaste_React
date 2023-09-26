e2d2a9d55b982d7fcd7104edc28a4020b2dba37e
Aditya <adityad@apra.in>
2023-09-26 17:35:23 +0530

    Episode 1 completed

--- Inception/app.js
+++ Inception/app.js
@@ -1,5 +1,16 @@
-const heading = React.createElement("h1", {id:"heading"}, "Hello World from React");
+//creating element using React 
+// const heading = React.createElement("h1", {id:"heading"}, "Hello World from React");
+
+//creating complex structure using React
+const container = React.createElement("div", { id: "parent" }, [
+    React.createElement("div", { id: "child1" }, [
+        React.createElement("p", {}, "I'm a para1"),
+        React.createElement("p", {}, "I'm a para2")]),
+    React.createElement("div", { id: "child2" }, [
+        React.createElement("p", {}, "I'm a para1"),
+        React.createElement("p", {}, "I'm a para2")])
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(container);
